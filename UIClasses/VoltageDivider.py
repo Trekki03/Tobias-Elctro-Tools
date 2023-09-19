f8882fcb5de8e53f5673e252f4797d26b01a2dee
Trekki03 <33525119+Trekki03@users.noreply.github.com>
2023-09-19 20:18:42 +0200

    Fixed Input Fields and added rounding

--- UIClasses/VoltageDivider.py
+++ UIClasses/VoltageDivider.py
@@ -1,6 +1,7 @@
 import UIFiles.UI_VoltageDivider
 from PySide6.QtWidgets import QWidget, QLineEdit
 from PySide6.QtCore import Qt, QEvent
+from PySide6.QtGui import QDoubleValidator
 
 class VoltageDivider(QWidget):
     def __init__(self):
@@ -9,26 +10,33 @@ class VoltageDivider(QWidget):
         self.uiVoltageDivider.setupUi(self)
         self.uiVoltageDivider.btCalculateUd.clicked.connect(self.actionCalculateUd)
         self.uiVoltageDivider.btCalculateR2.clicked.connect(self.actionCalculateR2)
+
         self.uiVoltageDivider.leUdU.installEventFilter(self)
+        self.uiVoltageDivider.leUdU.setValidator(QDoubleValidator().setNotation(QDoubleValidator.StandardNotation))
         self.uiVoltageDivider.leUdR1.installEventFilter(self)
+        self.uiVoltageDivider.leUdR1.setValidator(QDoubleValidator().setNotation(QDoubleValidator.StandardNotation))
         self.uiVoltageDivider.leUdR2.installEventFilter(self)
+        self.uiVoltageDivider.leUdR2.setValidator(QDoubleValidator().setNotation(QDoubleValidator.StandardNotation))
         self.uiVoltageDivider.leR2U.installEventFilter(self)
+        self.uiVoltageDivider.leR2U.setValidator(QDoubleValidator().setNotation(QDoubleValidator.StandardNotation))
         self.uiVoltageDivider.leR2R1.installEventFilter(self)
+        self.uiVoltageDivider.leR2R1.setValidator(QDoubleValidator().setNotation(QDoubleValidator.StandardNotation))
         self.uiVoltageDivider.leR2Ud.installEventFilter(self)
+        self.uiVoltageDivider.leR2Ud.setValidator(QDoubleValidator().setNotation(QDoubleValidator.StandardNotation))
         self.show()
 
     def actionCalculateUd(self):
-        u = int(self.uiVoltageDivider.leUdU.text())
-        r1 = int(self.uiVoltageDivider.leUdR1.text())
-        r2 = int(self.uiVoltageDivider.leUdR2.text())
-        ud = u/(r1+r2)*r2
+        u = float(self.uiVoltageDivider.leUdU.text())
+        r1 = float(self.uiVoltageDivider.leUdR1.text())
+        r2 = float(self.uiVoltageDivider.leUdR2.text())
+        ud = round(u/(r1+r2)*r2, 3)
         self.uiVoltageDivider.lbUdUd.setText(str(ud))
 
     def actionCalculateR2(self):
-        u = int(self.uiVoltageDivider.leR2U.text())
-        r1 = int(self.uiVoltageDivider.leR2R1.text())
-        ud = int(self.uiVoltageDivider.leR2Ud.text())
-        r2 = (ud*r1)/(u-ud)
+        u = float(self.uiVoltageDivider.leR2U.text())
+        r1 = float(self.uiVoltageDivider.leR2R1.text())
+        ud = float(self.uiVoltageDivider.leR2Ud.text())
+        r2 = round((ud*r1)/(u-ud), 3)
         self.uiVoltageDivider.lbR2R2.setText(str(r2))
 
     def eventFilter(self, source, event):
